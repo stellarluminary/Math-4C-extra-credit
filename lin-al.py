c97589742da136d474b55059fda8047de49b865f
Justine Wang <justinewang@Mac.attlocal.net>
2026-03-29 16:15:06 -0700

    Created key and matrix for encryption

---
+++ lin-al.py
@@ -0,0 +1,39 @@
+import numpy
+
+x = numpy.array([[1,3,1], [1,0,0] ])
+y = numpy.array([[0,0], [7,5], [1,1]])
+
+arr_1 = numpy.array([
+    [2,1,1],
+    [1,2,3]
+])
+
+arr_2 = numpy.array([
+    [0,2,3],
+    [4,0,0]
+])
+
+arr_3 = ([
+    [5,1,5],
+    [4,3,2]
+])
+
+# print(arr_1)
+# print(arr_2)
+
+# print(arr_1+arr_2)
+
+# try:
+#     x+y
+# except:
+#     print("Can only add matrices of the same size.")
+
+key = {" ":0, "A":1, "B":2,"C":3, "D":4, "E":5, "F": 6, 'G':7, 'H':8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15,\
+       'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z':26}
+
+for k,v in key.items():
+    if v == 24:
+        print(k)
+    else:
+        print("Not Xß")
+
