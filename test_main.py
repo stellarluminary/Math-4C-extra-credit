1c50c7fd658abefa72027cfede11cf857d9c72e0
Justine Wang <justinelwang@gmail.com>
2026-03-31 23:14:17 -0700

    Added first attempt at using pytest to test my main code

---
+++ test_main.py
@@ -0,0 +1,10 @@
+import pytest
+from main import main
+
+# def test_get_message(monkeypatch):
+#     monkeypatch.setattr("main.get_message", lambda _: "HOUSE")
+#     m = get_message()
+#     assert m == "HOUSE"
+
+# def test_numerize_message():
+#     assert numerize_message()
