67b74865f60ee0e75c244b80bb70a7a9246a5dae
Justine Wang <justinelwang@gmail.com>
2026-04-15 22:01:57 -0700

    Wrote test_numerize_msg and test_vectorize_numerized_msg in TestMessage class

--- test_main.py
+++ test_main.py
@@ -4,15 +4,17 @@ from message import Message
 # @pytest.fixture
 # msg = Message("EXPERIENCE")
 
+class TestMessage:
 
-def test_numerize_message():
-    msg = Message("EXPERIENCE")
-    assert msg.numerize_message() == [5, 24, 16, 5, 18, 9, 5, 14, 3, 5]
+    def test_numerize_message(self):
+        m = Message("EXPERIENCE")
+        nm = m.numerize_message()
+        assert nm == [5, 24, 16, 5, 18, 9, 5, 14, 3, 5]
 
-# def test_get_message(monkeypatch):
-#     monkeypatch.setattr("main.get_message", lambda _: "HOUSE")
-#     m = get_message()
-#     assert m == "HOUSE"
+    def test_vectorize_numerized_msg(self):
+        m = Message("LINEAR ALGEBRA")
+        m.numerize_message()
+        vm = m.vectorize_numerized_msg()
+        assert vm ==  [[12, 9, 14], [5, 1, 18], [0, 1, 12], [7, 5, 2], [18, 1, 0]]
 
-# def test_numerize_message():
-#     assert numerize_message()
+
